4735398c75ee675497d4c2f535f652e89927d3bd
Rafael Dantas <89138596+Danzokka@users.noreply.github.com>
2023-02-20 23:41:05 -0300

    Add files via upload

--- game.js
+++ game.js
@@ -56,6 +56,8 @@ canvas.addEventListener("click", function(evt){
             break;
         case estado.gameOver:
             let rect = canvas.getBoundingClientRect();
+            let clickX = evt.clientX - rect.left;
+            let clickY = evt.clientY - rect.top;
             
             // checa se o clique foi no botão START
             if(clickX >= startBtn.x && clickX <= startBtn.x + startBtn.w && clickY >= startBtn.y && clickY <= startBtn.y + startBtn.h){
